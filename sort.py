e530b3f84d27aa5fdc1ec21962f3a60f7788fc75
John Renzo S. Sunico <r.sunico@klab.com>
2016-12-13 15:30:21 +0800

    Added quicksort
    I'm not sure if my implementation is correct but it does the job, so, yeah.

--- sort.py
+++ sort.py
@@ -15,3 +15,25 @@ def insertion(unsorted):
             unsorted[position] = unsorted[position-1]
             position = position -1
         unsorted[position] = currentValue
+
+
+def quicksort(unsorted):
+    if len(unsorted) > 1:
+        pivot = unsorted[0]
+        left = []
+        middle = []
+        right = []
+        for val in unsorted:
+            if val < pivot:
+                left.append(val)
+            elif val == pivot:
+                middle.append(val)
+            elif val > pivot:
+                right.append(val)
+        left = quicksort(left)
+        middle = quicksort(middle)
+        right = quicksort(right)
+        return left + middle + right
+    else:
+        return unsorted
+    
