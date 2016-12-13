e4dea9fc52faf6ba77ea8841abb927d16016c037
John Renzo S. Sunico <r.sunico@klab.com>
2016-12-13 14:38:17 +0800

    Added insertion sort.

--- sort.py
+++ sort.py
@@ -4,3 +4,14 @@ def bubble_sort(unsorted):
             if j+1 < len(unsorted):
                 if unsorted[j+1] < unsorted[j]:
                     unsorted[j+1], unsorted[j] = unsorted[j], unsorted[j+1]
+
+
+def insertion(unsorted):
+    for index in range(1, len(unsorted)):
+        currentValue = unsorted[index]
+        position = index
+
+        while position > 0 and unsorted[position-1] > currentValue:
+            unsorted[position] = unsorted[position-1]
+            position = position -1
+        unsorted[position] = currentValue
